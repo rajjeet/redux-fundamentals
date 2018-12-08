7d291020c152cfc04ff414ec6afebcf51e10b81c
Rajjeet Phull <rajjeet.phull@gmail.com>
2018-12-08 10:46:24 -0500

    Seperated feestable component from conversion component

---
+++ public/js/components/FeesTable.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const FeesTable = ({conversionRate, fee, total, originCurrency, destinationCurrency}) => (
+    <div>
+        <table>
+            <tbody>
+            <tr>
+                <td>Conversion Rate</td>
+                <td>1 {originCurrency} -> {conversionRate.toFixed(2)} {destinationCurrency}</td>
+            </tr>
+            <tr>
+                <td>Fee</td>
+                <td>{fee.toFixed(2)} {originCurrency}</td>
+            </tr>
+            <tr>
+                <td className="total-label">Total Cost</td>
+                <td>{total.toFixed(2)} {originCurrency}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+);
+
+FeesTable.propTypes = {
+    conversionRate: PropTypes.number.isRequired,
+    originCurrency: PropTypes.string.isRequired,
+    total: PropTypes.number.isRequired,
+    destinationCurrency: PropTypes.string.isRequired
+};
+
+export default FeesTable;
